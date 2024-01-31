505ad284bfc517650c610544a4424955c23284d6
VladyslavStolbov <stolbovvladyslavcg@gmail.com>
2024-01-31 09:08:50 +0200

    Started implementing game over condition

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,10 @@
-using TMPro;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public int Score;
+    public int Score { get; private set; }
+    public int Lives;
     public TMP_Text Text;
 
     public void AddScore(int amount)
